9afb510b926516e09d2581dbf32c6c6496c190da
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 18:15:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,5 +28,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   intersection = valid_calls & planeteer_calls
   intersection[0]
+  # so a couple of loops
 
 end
